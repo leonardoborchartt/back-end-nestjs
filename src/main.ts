6dc2d316fcda20808583707623bb4bd9c0f29468
Leonardo Borchartt <leob_b@hotmail.com>
2020-04-02 16:56:09 -0300

    gitignore

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 const document = SwaggerModule.createDocument(app, options);
 SwaggerModule.setup('api', app, document);
 
-await app.listen(3333);
+await app.listen(process.env.PORT || 3333);
 console.log(`Application is running on: ${await app.getUrl()}`);
 
 }
